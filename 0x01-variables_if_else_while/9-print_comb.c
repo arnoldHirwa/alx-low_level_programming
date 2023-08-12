a4fe8f070bab17330357c47018629ffeccc6f521
Hirwa Arnold <arnoldhirw@gmail.com>
2023-08-11 19:06:26 -0700

    Add numbers and commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,30 +3,25 @@
 #include <stdio.h>
 
 /**
- *main - function that returns the last digit
+ *main - Print numbers followed by commas
  *
  * Return: Always (success)
  */
 int main(void)
 {
-	int n, lastdig;
+	int c;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	lastdig = n % 10;
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
 
-	if (lastdig > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastdig);
-	}
-	else if (lastdig == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, lastdig);
-	}
-	else if (lastdig < 6 && lastdig != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
+	putchar('\n');
 	return (0);
 }
