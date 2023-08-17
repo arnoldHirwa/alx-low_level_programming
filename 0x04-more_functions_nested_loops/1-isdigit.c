07770153179971adf415a3e546b8c97e1a38b29e
Hirwa Arnold <arnoldhirw@gmail.com>
2023-08-17 13:34:49 -0700

    fix: betty style

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
   * _isdigit - Check if a character is a digit
-  * @x: The number to be checked
+  * @y: The number to be checked
   *
   * Return: 1 for a character that will be a digit or 0 for any else
   */
