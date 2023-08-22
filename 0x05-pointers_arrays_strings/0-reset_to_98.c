19919910067490fa5c306b94ae237718be836e95
Hirwa Arnold <arnoldhirw@gmail.com>
2023-08-21 20:18:55 -0700

    Fix: betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - updates the value of the variable
  * the pointer points to to 98
- *e@ni: pointer to the variable to update
+ *@n: pointer to the variable to update
  */
 void reset_to_98(int *n)
 {
