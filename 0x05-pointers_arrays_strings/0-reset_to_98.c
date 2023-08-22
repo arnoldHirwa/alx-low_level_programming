78a4d83fd2dcc08a19d5d2bbfd4d0e852c06d717
Hirwa Arnold <arnoldhirw@gmail.com>
2023-08-21 20:01:19 -0700

    Learn pointers, arrays, and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**ii
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ *e@ni: pointer to the variable to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
