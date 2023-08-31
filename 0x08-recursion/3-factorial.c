59975e736e7062c6e1189352b9208353878ac81a
Hirwa Arnold <arnold.hirw@gmail.com>
2023-08-31 05:08:59 +0200

    Leant recursion in c with many exercises

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - Calculates factorial number.
+ * @n: number to calculate factorial.
+ * Return: facorial number
+ */
+int factorial(int n)
+{
+	if (n == 0 || n == 1)
+		return (1);
+	if (n > 1)
+		return (n * factorial(n - 1));
+	else
+		return (-1);
+}
