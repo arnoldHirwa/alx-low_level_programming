9122730b95da48b6c50b515be6d6acd18308bfa1
Hirwa Arnold <arnold.hirw@gmail.com>
2023-08-31 05:16:13 +0200

    Fix: included is_palindrome funtion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,29 +1,50 @@
-#include "main.h"
 /**
- * find_multipliers - look for multipliers of n.
- * @n: base number.
- * @i: iterator number.
- * Return: 1 if n is prime, 0 otherwise.
+ * _strlen_recursion - Find the length of a string
+ * @s: The string to find length of
+ * Return: The length
  */
-int find_multipliers(int n, int i)
+int _strlen_recursion(char *s)
 {
-	if (i == n)
-		return (1);
-	if (n % i == 0)
-		return (0);
-	else
-		return (find_multipliers(n, i + 1));
+	int l;
+
+	l = 0;
+	if (*s != 0)
+	{
+		l++;
+		return (l + _strlen_recursion(s + 1));
+	}
+	return (l);
 }
+
 /**
- * is_prime_number - check if n is prime
- * @n: base number.
+ * pal_check - Actual check for palindrome
+ * @s: String to check
+ * @l: Length of string
  *
- * Return: 1 if n is prime, 0 otherwise.
+ * Return: 1 if palindrome, 0 if otherwise
  */
-int is_prime_number(int n)
+int pal_check(char *s, int l)
 {
-	if (n <= 1)
+	if (l <= 1)
+		return (1);
+	if (*s != s[l - 1])
 		return (0);
 	else
-		return (find_multipliers(n, 2));
+		return (pal_check((s + 1), (l - 2)));
+}
+
+/**
+ * is_palindrome - Check if a string is a palindrome
+ * @s: String to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = _strlen_recursion(s);
+	if (l <= 1)
+		return (1);
+	return (pal_check(s, l));
 }
