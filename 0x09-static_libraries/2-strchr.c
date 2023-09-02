f53061200c3d29f4669028e12860ee59c24dbd0b
Hirwa Arnold <arnold.hirw@gmail.com>
2023-09-02 03:29:13 +0200

    Learn static libraries

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: the string to check
+ * @c: the character we're looking for
+ *
+ * Return: a pointer to the first occurance of the character @c in the string
+ * @s. Return NULL if the character isn't found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,4 @@
+LEARNING OBJECTIVES:
+
+-What is a static library, how does it work, how to create one, and how to use it
+-Basic usage of ar, ranlib, nm
