d4688d3ed02aa4c6b4d3a5c1d361dd7ea715e1c9
Hirwa Arnold <arnold.hirw@gmail.com>
2023-09-19 22:27:27 +0200

    Fix: Add header file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - sums all arguments passed
@@ -10,11 +11,11 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum;
+	int sum = 0;
 	va_list list;
 
 	va_start(list, n);
-	for (i = 0, sum = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(list, int);
 
 	va_end(list);
