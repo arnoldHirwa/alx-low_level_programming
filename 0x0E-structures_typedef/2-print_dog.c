6224f5fd10a6ad5b9221369bb17d2c693fe1c2f0
Hirwa Arnold <arnold.hirw@gmail.com>
2023-09-19 03:19:48 +0200

    Fix: handle null values

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
 * print_dog - prints a struct dog
@@ -8,22 +10,21 @@
 
 void print_dog(struct dog *d)
 {
-if (d == NULL )
-return; 
+	if (d != NULL)
+	{
+		if ((*d).name == NULL)
+		printf("Name: nil\n");
+		else
+		printf("Name: %s\n", d->name);
 
-if ((*d).name == NULL)
-printf("nil\n");
-else
-printf("Name: %s\n", d->name);
+		if (!(*d).age)
+		printf("Age: nil\n");
+		else
+		printf("Age: %f\n", d->age);
 
-if ((*d).age < 0)
-printf("nil\n");
-else
-printf("Age: %f\n", d->age);
-
-if ((*d).owner == NULL)
-printf("nil\n");
-else
-printf("Owner: %s\n", d->owner);
-
+		if ((*d).owner == NULL)
+		printf("Owner: nil\n");
+		else
+		printf("Owner: %s\n", d->owner);
+	}
 }
