40c9382d0cdb174cc2bc27f8cff3060eb6f08dad
Hirwa Arnold <arnold.hirw@gmail.com>
2023-09-28 00:19:00 +0200

    Add meaningful function name

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) hare(void);
+void __attribute__((constructor)) first(void);
 
 /**
- * hare - Prints a string before the
+ * first - Prints a string before the
  *   main function is executed.
  */
-void hare(void)
+void first(void)
 {
 printf("You're beat! and yet, you must allow,\n"
 "I bore my house upon my back!\n");
