32b6a0165cf30088a676e301c88f2a3ddbfc1cd1
Hirwa Arnold <arnold.hirw@gmail.com>
2023-10-03 05:05:09 +0200

    Consider returning before many errors occur

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,40 +2,49 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
- */
+* read_textfile - Reads a text file and prints it to POSIX stdout.
+* @filename: A pointer to the name of the file.
+* @letters: The number of letters the
+*           function should read and print.
+*
+* Return: If the function fails or filename is NULL - 0.
+*         O/w - the actual number of bytes the function can read and print.
+*/
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	 ssize_t o, r, w;
+	ssize_t r, w, o;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer ==  NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (buffer == NULL)
 	{
 		free(buffer);
 		return (0);
 	}
 
+	o = open(filename, O_RDONLY);
+	if (o == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	r = read(o, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buffer, letters);
+	if (w == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 	free(buffer);
-	close(o);
-
 	return (w);
 
 }
