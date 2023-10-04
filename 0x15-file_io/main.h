ab60f79a21ce2c75a3ad346886969609141a1e54
Hirwa Arnold <arnold.hirw@gmail.com>
2023-10-04 21:48:24 +0200

    add close function

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,5 +14,6 @@ int append_text_to_file(const char *filename, char *text_content);
 void _copy(char *file_from, char *file_to);
 int main(int argc, char **av);
 int _putchar(char c);
+void close_file(int fd);
 
 #endif /*ifndef MAIN_H*/
