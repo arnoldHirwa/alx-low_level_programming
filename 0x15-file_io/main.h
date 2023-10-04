9066c713bfcc5ebd55bff669faef52e033d38902
Hirwa Arnold <arnold.hirw@gmail.com>
2023-10-04 21:36:27 +0200

    Add main function

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void _copy(char *file_from, char *file_to);
+int main(int argc, char **av);
 int _putchar(char c);
 
 #endif /*ifndef MAIN_H*/
