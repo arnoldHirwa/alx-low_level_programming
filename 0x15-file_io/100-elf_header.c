254d1197635805f7c777342a6b03889e5119e947
Hirwa Arnold <arnold.hirw@gmail.com>
2023-10-04 22:37:13 +0200

    debug last 2 functions

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -225,8 +225,8 @@ void print_type(unsigned int e_type, unsigned char *ident)
 
 /**
  * print_entry - Prints the entry point of an ELF header.
- * @e_entry: The address of the ELF entry point.
- * @e_ident: A pointer to an array containing the ELF class.
+ * @entry: The address of the ELF entry point.
+ * @ident: A pointer to an array containing the ELF class.
  */
 void print_entry(unsigned long int entry, unsigned char *ident)
 {
