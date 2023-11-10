9bedcd250ba3ee0787cb05c77c4255d8ad2c53b9
Hirwa Arnold <arnold.hirw@gmail.com>
2023-11-10 04:49:47 +0200

    Consider edge cases

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,24 +1,33 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns number of elements in a linked dlistint_t list.
- * @h: pointer to the head of the list
- *
- * Return: number of nodes
- */
+* dlistint_len - returns number of elements in a linked dlistint_t list.
+* @h: pointer to the head of the list
+*
+* Return: number of nodes
+*/
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count = 0;
+	size_t len = 0;
 
 	if (h == NULL)
-		return (count);
-
+	{
+		return (len);
+	}
+	if (h->prev != NULL)
+	{
+		h = h->prev;
+		while (h->prev != NULL)
+		{
+			h = h->prev;
+		}
+	}
 	while (h != NULL)
 	{
-		count++;
 		h = h->next;
+		len++;
 	}
 
-	return (count);
+	return (len);
 }
