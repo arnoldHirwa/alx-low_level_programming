7dab5d76f61a8f30b42fe30d78ced99a9cc02d1f
Hirwa Arnold <arnold.hirw@gmail.com>
2023-12-02 04:45:09 +0200

    Learn hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - creates a hash table
+* @size: the size of the array
+*
+* Return: pointer to the new created table or null if it failed
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	hash_node_t **array = malloc(sizeof(hash_node_t) * size);
+
+	if (new_table == NULL || array == NULL)
+	{
+		if (new_table)
+			free(new_table);
+		if (array)
+			free(array);
+		return (NULL);
+	}
+
+	new_table->size = size;
+	new_table->array = array;
+
+	return (new_table);
+}
